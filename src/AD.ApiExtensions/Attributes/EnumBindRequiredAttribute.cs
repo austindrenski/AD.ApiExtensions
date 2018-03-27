6364580bc89d9bcdcd19d17c62d8304b29518f4c
Austin Drenski <austin.drenski@gmail.com>
2018-03-27 11:19:58 -0400

    adding EnumBindRequiredAttribute which requires a non-default enum value

---
+++ src/AD.ApiExtensions/Attributes/EnumBindRequiredAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace AD.ApiExtensions.Attributes
+{
+    /// <inheritdoc cref="ValidationAttribute" />
+    /// <summary>
+    /// Requires that an enum parameter be bound against a non-default value.
+    /// </summary>
+    [PublicAPI]
+    public class EnumBindRequiredAttribute : ValidationAttribute
+    {
+        /// <inheritdoc />
+        protected override ValidationResult IsValid([NotNull] object value, [NotNull] ValidationContext validationContext)
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (validationContext is null)
+            {
+                throw new ArgumentNullException(nameof(validationContext));
+            }
+
+            if (!value.GetType().IsEnum)
+            {
+                throw new ArgumentException("Enumeration type required.");
+            }
+
+            return
+                (long) value is default
+                    ? new ValidationResult($"Expected non-default enumeration, but received: {value}")
+                    : ValidationResult.Success;
+        }
+    }
+}
