5382f7086db2bb12ebea8277d9b4e817e1c00b21
Austin Drenski <austin.drenski@gmail.com>
2018-04-08 16:29:21 -0400

    adding types from other

---
+++ src/AD.ApiExtensions/Http/ConflictResult.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace AD.ApiExtensions.Http
+{
+    /// <summary>
+    /// An <see cref="ActionResult" /> that produces a <see cref="StatusCodes.Status409Conflict" /> response.
+    /// </summary>
+    [PublicAPI]
+    public class ConflictResult : StatusCodeResult
+    {
+        /// <summary>
+        /// Constructs a <see cref="ConflictResult" /> that produces a <see cref="StatusCodes.Status409Conflict" /> response.
+        /// </summary>
+        public ConflictResult() : base(StatusCodes.Status409Conflict) { }
+    }
+}
