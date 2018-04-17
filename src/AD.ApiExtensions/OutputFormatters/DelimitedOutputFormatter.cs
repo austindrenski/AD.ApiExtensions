9f54943e06404a53dc43ae255abf2e1fb37e2ec5
Austin Drenski <austin.drenski@gmail.com>
2018-04-17 11:27:27 -0400

    wip -- not stable

--- src/AD.ApiExtensions/OutputFormatters/DelimitedOutputFormatter.cs
+++ src/AD.ApiExtensions/OutputFormatters/DelimitedOutputFormatter.cs
@@ -1,41 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using AD.IO;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
 
 namespace AD.ApiExtensions.OutputFormatters
 {
-    /// <inheritdoc />
+    /// <inheritdoc cref="IOutputFormatter"/>
     /// <summary>
     /// Writes an object in delimited format to the output stream.
     /// </summary>
     [PublicAPI]
-    public class DelimitedOutputFormatter : IOutputFormatter
+    public class DelimitedOutputFormatter : IOutputFormatter, IApiResponseTypeMetadataProvider
     {
         /// <summary>
         /// The collection of supported media types.
         /// </summary>
-        [NotNull] private static readonly Dictionary<MediaTypeHeaderValue, char> SupportedMediaTypes;
+        [NotNull]
+        public IDictionary<string, char> SupportedMediaTypes { get; }
 
         /// <summary>
         /// Initializes static resources.
         /// </summary>
-        static DelimitedOutputFormatter()
+        public DelimitedOutputFormatter()
         {
             SupportedMediaTypes =
-                new Dictionary<MediaTypeHeaderValue, char>
+                new Dictionary<string, char>
                 {
-                    [MediaTypeHeaderValue.Parse("text/csv")] = ',',
-                    [MediaTypeHeaderValue.Parse("text/psv")] = '|',
-                    [MediaTypeHeaderValue.Parse("text/tab-separated-values")] = '\t'
+                    ["text/csv; charset=utf-8"] = ',',
+                    ["text/psv; charset=utf-8"] = '|',
+                    ["text/tab-separated-values; charset=utf-8"] = '\t'
                 };
+        }
+
+        /// <inheritdoc />
+        public IReadOnlyList<string> GetSupportedContentTypes([NotNull] string contentType, [NotNull] Type objectType)
+        {
+            if (contentType is null)
+            {
+                throw new ArgumentNullException(nameof(contentType));
+            }
+
+            if (objectType is null)
+            {
+                throw new ArgumentNullException(nameof(objectType));
+            }
+
+            if (!SupportedMediaTypes.Any())
+            {
+                throw new InvalidOperationException($"{nameof(DelimitedOutputFormatter)} does not support any media types.");
+            }
+
+            List<MediaType> results = new List<MediaType>();
+
+            MediaType contentMediaType = new MediaType(contentType);
+
+            foreach ((string mediaType, char _) in SupportedMediaTypes)
+            {
+                MediaType supportedMediaType = new MediaType(mediaType);
+
+                if (supportedMediaType.HasWildcard && contentMediaType.IsSubsetOf(supportedMediaType))
+                {
+                    results.Add(contentMediaType);
+                }
+                else if (supportedMediaType.IsSubsetOf(contentMediaType))
+                {
+                    results.Add(supportedMediaType);
+                }
+            }
+
+            return results.Select(x => $"{x.Type}/{x.SubType}").ToArray();
+        }
+
+        /// <summary>
+        /// Determines whether this <see cref="IOutputFormatter" /> can produce the specified <paramref name="contentType"/>.
+        /// </summary>
+        /// <param name="contentType">
+        /// The content type to check.
+        /// </param>
+        /// <returns>
+        /// True if the formatter can write the response; otherwise, false.
+        /// </returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        [Pure]
+        public bool CanWriteResult(StringSegment contentType)
+        {
+            if (!SupportedMediaTypes.Any())
+            {
+                throw new InvalidOperationException($"{nameof(DelimitedOutputFormatter)} does not support any media types.");
+            }
+
+            MediaType contentMediaType = new MediaType(contentType);
+
+            foreach ((string mediaType, char _) in SupportedMediaTypes)
+            {
+                MediaType supportedMediaType = new MediaType(mediaType);
+
+                if (supportedMediaType.HasWildcard && contentMediaType.IsSubsetOf(supportedMediaType))
+                {
+                    return true;
+                }
+
+                if (supportedMediaType.IsSubsetOf(contentMediaType))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         /// <inheritdoc />
@@ -48,7 +127,7 @@ namespace AD.ApiExtensions.OutputFormatters
             }
 
             return
-                SupportedMediaTypes.Keys.Contains(MediaTypeHeaderValue.Parse(context.ContentType)) ||
+                CanWriteResult(context.ContentType) ||
                 context.HttpContext.Request.Headers["user-agent"].Any(x => x?.StartsWith("Stata") ?? false);
         }
 
@@ -60,23 +139,24 @@ namespace AD.ApiExtensions.OutputFormatters
                 throw new ArgumentNullException(nameof(context));
             }
 
-            char delimiter = SupportedMediaTypes[MediaTypeHeaderValue.Parse(context.ContentType)];
+            MediaType headerValue = new MediaType(context.ContentType.Value);
+            string text = GetDelimited(context.Object, SupportedMediaTypes[context.ContentType.Value]);
 
-            string text = GetDelimited(context.Object, delimiter);
-
+            context.HttpContext.Response.ContentType = context.ContentType.Value;
             context.HttpContext.Response.Headers.Add("header", "present");
-            context.HttpContext.Response.Headers.Add("charset", Encoding.UTF8.WebName);
-            context.HttpContext.Response.ContentType = MediaType.ReplaceEncoding(context.ContentType, Encoding.UTF8);
-            context.HttpContext.Response.StatusCode = StatusCodes.Status200OK;
-            await context.HttpContext.Response.WriteAsync(text, context.HttpContext.RequestAborted);
+            context.HttpContext.Response.Headers.Add("charset", headerValue.Encoding.WebName);
+            await context.HttpContext.Response.WriteAsync(text, headerValue.Encoding, context.HttpContext.RequestAborted);
         }
 
         /// <summary>
-        ///
+        /// Gets the delimited string representation of the value.
         /// </summary>
-        /// <param name="value"></param>
-        /// <param name="delimiter"></param>
-        /// <returns></returns>
+        /// <param name="value">
+        /// The value to serialize as a delimited string.
+        /// </param>
+        /// <param name="delimiter">
+        /// The delimiter character.
+        /// </param>
         [Pure]
         [NotNull]
         private static string GetDelimited([CanBeNull] object value, char delimiter)
--- test/AD.ApiExtensions.Tests/DelimitedOutputFormatterTests.cs
+++ test/AD.ApiExtensions.Tests/DelimitedOutputFormatterTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Xml.Linq;
 using AD.ApiExtensions.OutputFormatters;
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace AD.ApiExtensions.Tests
@@ -13,53 +15,112 @@ namespace AD.ApiExtensions.Tests
     public class DelimitedOutputFormatterTests
     {
         [Theory]
-        [InlineData("text/csv", ',')]
-        [InlineData("text/psv", '|')]
-        [InlineData("text/tab-separated-values", '\t')]
-        public void Test0(string contentType, char delimiter)
+        [InlineData("text/csv", "utf-8", ',')]
+        [InlineData("text/psv", "utf-8", '|')]
+        [InlineData("text/tab-separated-values", "utf-8", '\t')]
+        public void Test0(string contentType, string charset, char delimiter)
         {
-            DelimitedOutputFormatter formatter = new DelimitedOutputFormatter();
-
-            DefaultHttpContext context = new DefaultHttpContext();
-
-            context.Initialize(context.Features);
-
-            context.Request.Headers["Content-Type"] = contentType;
+            HttpContext context =
+                GetHttpContext(("Content-Type", contentType));
 
             OutputFormatterWriteContext outputContext =
-                new OutputFormatterWriteContext(
+                GetOutputFormatterWriteContext(
                     context,
-                    (s, e) => new StreamWriter(s, e),
-                    typeof(XElement[]),
                     new XElement[]
                     {
-                        new XElement("a", 1),
-                        new XElement("b", 2),
-                        new XElement("c", 3),
-                    })
-                {
-                    ContentType = context.Request.ContentType
-                };
+                        new XElement("record",
+                            new XElement("a", 1),
+                            new XElement("b", 2),
+                            new XElement("c", 3))
+                    });
 
+            DelimitedOutputFormatter formatter = new DelimitedOutputFormatter();
+
+            // Does the formatter accept the content type?
             Assert.True(formatter.CanWriteResult(outputContext));
 
             formatter.WriteAsync(outputContext).Wait();
 
-            Assert.Equal($"{contentType}; charset=utf-8", context.Response.Headers["Content-Type"]);
-            Assert.Equal($"{contentType}; charset=utf-8", context.Response.ContentType);
-            Assert.Equal("utf-8", context.Response.Headers["charset"]);
+            // Did the formatter set the response headers?
+            Assert.Equal($"{contentType}; charset={charset}", context.Response.Headers["Content-Type"]);
+            Assert.Equal(charset, context.Response.Headers["charset"]);
             Assert.Equal("present", context.Response.Headers["header"]);
             Assert.Equal(StatusCodes.Status200OK, context.Response.StatusCode);
 
-            using (StreamReader reader = new StreamReader(context.Response.Body))
-            {
-                string expected =
-                    string.Join(
-                        Environment.NewLine,
-                        $"a{delimiter}b{delimiter}c",
-                        $"1{delimiter}2{delimiter}3");
+            // Did the formatter write the response to the body?
+            string expected =
+                string.Join(Environment.NewLine,
+                    $"a{delimiter}b{delimiter}c",
+                    $"1{delimiter}2{delimiter}3");
 
-                Assert.Equal(expected, reader.ReadToEnd());
+            Assert.Equal(expected, GetBodyString(context.Response));
+        }
+
+        [Pure]
+        [NotNull]
+        public static HttpContext GetHttpContext(params (string Header, string Value)[] headers)
+        {
+            DefaultHttpContext context = new DefaultHttpContext();
+
+            foreach ((string header, string value) in headers)
+            {
+                context.Request.Headers.Add(header, value);
+            }
+
+            context.Response.Body = new MemoryStream();
+
+            return context;
+        }
+
+        [Pure]
+        [NotNull]
+        public static OutputFormatterWriteContext GetOutputFormatterWriteContext<T>([NotNull] HttpContext context, [NotNull] T value)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return
+                new OutputFormatterWriteContext(context, (s, e) => new StreamWriter(s, e), typeof(T), value)
+                {
+                    ContentType = context.Request.ContentType
+                };
+        }
+
+        /// <summary>
+        /// Reads the response body with the encoding specified by the content type header.
+        /// </summary>
+        /// <param name="response">
+        /// The response from which to read.
+        /// </param>
+        /// <returns>
+        /// The string value of the response body.
+        /// </returns>
+        /// <exception cref="ArgumentNullException" />
+        [NotNull]
+        public static string GetBodyString([NotNull] HttpResponse response)
+        {
+            if (response is null)
+            {
+                throw new ArgumentNullException(nameof(response));
+            }
+
+            if (response.Body.CanSeek)
+            {
+                response.Body.Seek(default, SeekOrigin.Begin);
+            }
+
+            Encoding encoding = MediaTypeHeaderValue.Parse(response.ContentType).Encoding;
+
+            using (StreamReader reader = new StreamReader(response.Body, encoding))
+            {
+                return reader.ReadToEnd();
             }
         }
     }
