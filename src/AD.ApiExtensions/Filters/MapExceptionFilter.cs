ca4b8f0644500fc16efac7a2d7e34787fb92f129
Austin Drenski <austin.drenski@gmail.com>
2018-04-18 18:43:01 -0400

    operation filter

---
+++ src/AD.ApiExtensions/Filters/MapExceptionFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AD.ApiExtensions.Filters
+{
+    /// <inheritdoc cref="IAsyncActionFilter"/>
+    /// <inheritdoc cref="IExceptionFilter"/>
+    /// <inheritdoc cref="IOrderedFilter"/>
+    /// <summary>
+    /// Handles a <typeparamref name="TException"/> by returning a <see cref="TResult"/>.
+    /// </summary>
+    [PublicAPI]
+    public class MapExceptionFilter<TException, TResult>
+        : IAsyncExceptionFilter,
+          IExceptionFilter,
+          IOrderedFilter
+        where TException : Exception
+        where TResult : StatusCodeResult, new()
+    {
+        /// <inheritdoc />
+        public int Order { get; set; }
+
+        /// <inheritdoc />
+        public virtual void OnException([NotNull] ExceptionContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (!(context.Exception is TException))
+            {
+                return;
+            }
+
+            context.ExceptionHandled = true;
+            context.Result = new TResult();
+        }
+
+        /// <inheritdoc />
+        public virtual Task OnExceptionAsync([NotNull] ExceptionContext context)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            OnException(context);
+
+            return Task.CompletedTask;
+        }
+    }
+}
