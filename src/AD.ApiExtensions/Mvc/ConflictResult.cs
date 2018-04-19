6df46f5f30eb6bc4654d528e9a6f8a9c0ca42238
Austin Drenski <austin.drenski@gmail.com>
2018-04-19 13:42:09 -0400

    shuffling

---
+++ src/AD.ApiExtensions/Mvc/ConflictResult.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace AD.ApiExtensions.Mvc
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// An <see cref="T:Microsoft.AspNetCore.Mvc.ActionResult" /> that produces a <see cref="F:Microsoft.AspNetCore.Http.StatusCodes.Status409Conflict" /> response.
+    /// </summary>
+    [PublicAPI]
+    public class ConflictResult : StatusCodeResult
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Constructs a <see cref="T:AD.ApiExtensions.Http.ConflictResult" /> that produces a <see cref="F:Microsoft.AspNetCore.Http.StatusCodes.Status409Conflict" /> response.
+        /// </summary>
+        public ConflictResult() : base(StatusCodes.Status409Conflict) { }
+    }
+}
