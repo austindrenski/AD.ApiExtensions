4c5c0280ba58090acb9c1d411ab7648ee7f688a9
Austin Drenski <austin.drenski@gmail.com>
2018-09-18 09:36:49 -0400

    Document and cleanup `TypeCache` with changes
    - Includes changes to the recursive type walk for `MethodInfo`

--- src/AD.ApiExtensions/Expressions/TypeCache.cs
+++ src/AD.ApiExtensions/Expressions/TypeCache.cs
@@ -7,7 +7,6 @@ using JetBrains.Annotations;
 
 namespace AD.ApiExtensions.Expressions
 {
-    // TODO: document TypeCache
     /// <summary>
     /// Represents a cache of types and updates.
     /// </summary>
@@ -20,58 +19,76 @@ namespace AD.ApiExtensions.Expressions
         [NotNull] readonly Dictionary<Type, Type> _types = new Dictionary<Type, Type>();
 
         /// <summary>
-        ///
-        /// </summary>
-        [NotNull] readonly ISet<(string Name, Type Type)> _differences = new HashSet<(string Name, Type Type)>();
-
-        /// <summary>
         /// Caches anonymous types that were encountered and modified.
         /// </summary>
         [NotNull] readonly Dictionary<Type, ParameterExpression> _parameters = new Dictionary<Type, ParameterExpression>();
 
         /// <summary>
-        ///
+        /// Determines whether the <see cref="T:System.Collections.Generic.Dictionary`2"></see> contains the specified key.
         /// </summary>
-        /// <param name="type"></param>
+        /// <param name="type">The key to locate in the <see cref="T:System.Collections.Generic.Dictionary`2"></see>.</param>
         /// <returns>
-        ///
+        /// True if the <see cref="TypeCache"/> contains an element of the specified type; otherwise, false.
         /// </returns>
-        /// <exception cref="ArgumentNullException"><paramref name="type"/></exception>
         [Pure]
         [ContractAnnotation("type:null => false")]
         public bool ContainsKey([CanBeNull] Type type) => type != null && _types.ContainsKey(type);
 
         /// <summary>
-        ///
+        /// Returns an updated type or the initial type if no update is required.
         /// </summary>
-        /// <param name="methodInfo"></param>
+        /// <param name="type">The type to update.</param>
         /// <returns>
-        ///
+        /// An updated type or the initial type if no update is required.
         /// </returns>
-        /// <exception cref="ArgumentNullException"><paramref name="methodInfo"/></exception>
+        /// <exception cref="ArgumentNullException"><paramref name="type"/></exception>
         [Pure]
         [NotNull]
-        public MethodInfo GetOrUpdate([NotNull] MethodInfo methodInfo)
+        public Type GetOrUpdate([NotNull] Type type)
         {
-            if (methodInfo == null)
-                throw new ArgumentNullException(nameof(methodInfo));
+            if (type is null)
+                throw new ArgumentNullException(nameof(type));
 
-            return
-                !methodInfo.IsGenericMethod
-                    ? methodInfo
-                    : methodInfo.GetGenericMethodDefinition()
-                                .MakeGenericMethod(
-                                     methodInfo.GetGenericArguments()
-                                               .Select(GetTypeOrInput)
-                                               .ToArray());
+            return _types.TryGetValue(RecurseType(type), out Type result) ? result : type;
+
+            Type RecurseType(Type t)
+                => t.IsConstructedGenericType
+                       ? GetOrUpdate(t.GetGenericTypeDefinition().MakeGenericType(t.GenericTypeArguments.Select(RecurseType).ToArray()))
+                       : t;
         }
 
         /// <summary>
-        ///
+        /// Returns an updated method or the initial method if no update is required.
         /// </summary>
-        /// <param name="expression"></param>
+        /// <param name="method">The method to update.</param>
         /// <returns>
-        ///
+        /// An updated method or the initial method if no update is required.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref name="method"/></exception>
+        [Pure]
+        [NotNull]
+        public MethodInfo GetOrUpdate([NotNull] MethodInfo method)
+        {
+            if (method == null)
+                throw new ArgumentNullException(nameof(method));
+
+            return
+                method.IsGenericMethod
+                    ? method.GetGenericMethodDefinition()
+                            .MakeGenericMethod(
+                                 method.GetGenericArguments()
+                                       .Select(GetOrUpdate)
+                                       .ToArray())
+                    : method;
+        }
+
+        /// <summary>
+        /// Returns an updated <see cref="ParameterExpression"/> or the initial expression if no update
+        /// is required, or if the initial expression is not a <see cref="ParameterExpression"/>.
+        /// </summary>
+        /// <param name="expression">The expression to update.</param>
+        /// <returns>
+        /// An updated <see cref="ParameterExpression"/> or the initial expression if no update is required.
         /// </returns>
         /// <exception cref="ArgumentNullException"><paramref name="expression"/></exception>
         [Pure]
@@ -88,16 +105,15 @@ namespace AD.ApiExtensions.Expressions
                 return parameter;
 
             Register(expression.Type, expression.Type);
-            return _parameters[expression.Type];
+
+            return _parameters.TryGetValue(expression.Type, out parameter) ? parameter : expression;
         }
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="result"></param>
+        /// <summary>Gets the value associated with the specified key.</summary>
+        /// <param name="type">The type of the value to get.</param>
+        /// <param name="result">If this method returns true, contains the parameter associated with the specified type.</param>
         /// <returns>
-        ///
+        /// True if the <see cref="TypeCache"/> contains an element with the specified type; otherwise, false.
         /// </returns>
         /// <exception cref="ArgumentNullException"><paramref name="type"/></exception>
         [Pure]
@@ -111,11 +127,43 @@ namespace AD.ApiExtensions.Expressions
         }
 
         /// <summary>
+        /// Updates the initial type and adds it to the type and parameter cache, provided
+        /// that the initial type is a constructed generic type and that the type does not
+        /// already exist in the cache.
+        /// </summary>
+        /// <param name="type">The initial type.</param>
+        /// <param name="result">The result type.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="type"/></exception>
+        /// <exception cref="ArgumentNullException"><paramref name="result"/></exception>
+        public void Register([NotNull] Type type, [NotNull] Type result)
+        {
+            if (type == null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (result == null)
+                throw new ArgumentNullException(nameof(result));
+
+            if (!type.IsConstructedGenericType)
+                return;
+
+            if (_types.ContainsKey(type))
+                return;
+
+            Type updatedType = GetOrUpdate(result);
+
+            if (!_parameters.TryGetValue(updatedType, out ParameterExpression parameter))
+                parameter = Expression.Parameter(updatedType, $"param_{updatedType.Name}");
+
+            _types.Add(type, parameter.Type);
+            _parameters.Add(type, parameter);
+        }
+
+        /// <summary>
         /// Tests if it is provable that an assignment results in a default initialization.
         /// </summary>
+        /// <param name="memberInfo">The target of assignment.</param>
         /// <param name="expression">The expression for assignment.</param>
         /// <param name="unavailable">The property names that were explicitly removed by the time of testing.</param>
-        /// <param name="memberInfo">The target of assignment.</param>
         /// <returns>
         /// True if it is provable that the assignment results in a default initialization; otherwise false.
         /// </returns>
@@ -143,103 +191,14 @@ namespace AD.ApiExtensions.Expressions
             if (_types.Count == 0 || IsUpdatedMemberName(memberInfo.Name))
                 return false;
 
-//            // TODO: look into this to replace the current "unavailable" methodology.
-//                return _differences.Contains((memberInfo.Name, memberInfo.GetType()));
-            return unavailable.ContainsKey(memberInfo.Name) && unavailable[memberInfo.Name].GetType() == memberInfo.GetType();
+            return unavailable.TryGetValue(memberInfo.Name, out MemberInfo m) && m.GetType() == memberInfo.GetType();
+
+            bool IsUpdatedMemberName(string name)
+                => _types.Select(x => x.Value)
+                         .SelectMany(x => x.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+                         .Select(y => y.Name)
+                         .Contains(name);
         }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="result"></param>
-        /// <exception cref="ArgumentNullException"><paramref name="type"/></exception>
-        /// <exception cref="ArgumentNullException"><paramref name="result"/></exception>
-        public void Register([NotNull] Type type, [NotNull] Type result)
-        {
-            if (type == null)
-                throw new ArgumentNullException(nameof(type));
-
-            if (result == null)
-                throw new ArgumentNullException(nameof(result));
-
-            if (!type.IsConstructedGenericType)
-                return;
-
-            if (_types.ContainsKey(type))
-                return;
-
-            Type recurseType = RecurseType(result);
-
-            if (!_parameters.TryGetValue(recurseType, out ParameterExpression parameter))
-                parameter = Expression.Parameter(recurseType, $"param_{recurseType.Name}");
-
-            _types.Add(type, parameter.Type);
-            _parameters.Add(type, parameter);
-
-//            // TODO: look into this to replace the current "unavailable" methodology.
-//            IEnumerable<(string Name, Type PropertyType)> keys =
-//                _types.Select(x => x.Key)
-//                      .SelectMany(x => x.GetProperties(BindingFlags.FlattenHierarchy).Select(y => (y.Name, y.PropertyType)))
-//                      .Distinct();
-//
-//            IEnumerable<(string Name, Type PropertyType)> values =
-//                _types.Select(x => x.Value)
-//                      .SelectMany(x => x.GetProperties(BindingFlags.FlattenHierarchy).Select(y => (y.Name, y.PropertyType)))
-//                      .Distinct();
-//
-//            foreach ((string Name, Type PropertyType) d in keys.Except(values))
-//            {
-//                _differences.Add(d);
-//            }
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns>
-        ///
-        /// </returns>
-        [Pure]
-        [NotNull]
-        Type GetTypeOrInput([NotNull] Type type) => _types.TryGetValue(type, out Type result) ? result : type;
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns>
-        ///
-        /// </returns>
-        [Pure]
-        [NotNull]
-        Type RecurseType([NotNull] Type type)
-        {
-            if (!type.IsConstructedGenericType)
-                return type;
-
-            return
-                GetTypeOrInput(
-                    type.GetGenericTypeDefinition()
-                        .MakeGenericType(
-                             type.GenericTypeArguments
-                                 .Select(RecurseType)
-                                 .ToArray()));
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns>
-        ///
-        /// </returns>
-        bool IsUpdatedMemberName([NotNull] string name)
-            => _types.Select(x => x.Value)
-                     .SelectMany(x => x.GetProperties(BindingFlags.Instance | BindingFlags.Public))
-                     .Select(y => y.Name)
-                     .Contains(name);
 
         /// <summary>
         /// Tests if the item is recognized as the default value of the underlying type.
